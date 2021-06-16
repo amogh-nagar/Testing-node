2902f931be88624ccb0231ab6bc3151e48fe7e29
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2021-06-16 11:11:21 +0530

    don't check functions

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,12 +1,50 @@
 const authMiddleware=require('../middleware/is-auth')
 const expect=require('chai').expect
 
-it('should throw an error if no authorization heade is present',function(){
+
+
+describe('Auth middleware',function(){
+
+    it('should throw an error if no authorization heade is present',function(){
+        const req={
+            get:function(headername){
+              return null
+            }
+        }
+        expect(authMiddleware.bind(this,req,{},()=>{})).to.throw('Not authenticated.');//we are not calling it by ourself instead we are passing the function reference
+    //i want to let the testing frameworks to calll this function for me so that they can hendle the thrown error
+    })
+
+    
+it('should throw an error if the authorization header is only one string',function(){
     const req={
         get:function(headername){
-          return null
+          return 'xyz'
         }
     }
-    expect(authMiddleware.bind(this,req,{},()=>{})).to.throw('Not authenticated.');//we are not calling it by ourself instead we are passing the function reference
-//i want to let the testing frameworks to calll this function for me so that they can hendle the thrown error
+    expect(authMiddleware.bind(this,req,{},()=>{})).to.throw()
 })
+
+
+it('should throw an error if token cannot be verified',function(){
+    const req={
+        get:function(headername){
+          return 'Bearer xyz'
+        }
+    }
+    expect(authMiddleware.bind(this,req,{},()=>{})).to.throw()
+})
+
+
+it('should yeild a userId after decoding the token',function(){
+    const req={
+        get:function(headername){
+          return 'Bearer erirghsrtgkj'
+        }
+    }
+    authMiddleware(req,{},()=>{})
+    expect(req).to.have.property('userId')
+})
+})
+
+
